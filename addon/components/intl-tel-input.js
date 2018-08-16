6ad698afa10fb08087a0eb0d2f88a49e0d5d0115
Cyrille David <cyrille@qonto.eu>
2018-08-16 15:49:04 +0200

    Improve inline documentation

--- addon/components/intl-tel-input.js
+++ addon/components/intl-tel-input.js
@@ -1,9 +1,5 @@
 /* global intlTelInputUtils */
 
-/**
- * @module ember-phone-input
- */
-
 import Component from '@ember/component'
 import { assert } from '@ember/debug'
 import { attribute, tagName } from '@ember-decorators/component'
@@ -12,65 +8,80 @@ import $ from 'jquery'
 const PHONE_NUMBER_FORMAT = 'E164' // https://en.wikipedia.org/wiki/E.164
 
 /**
- * @class PhoneInput
- */
+  A phone-input component. Usage:
+  ```hbs
+    {{phone-input
+    number='123'
+    autoPlaceholder='aggressive'
+    initialCountry='fr'
+    onlyCountries=europeanCountries
+    preferredCountries=englishSpeakingCountries
+    update=(action 'handleUpdate')}}
+  ```
+
+  @class PhoneInput
+  @public
+*/
 @tagName('input')
 export default class IntlTelInput extends Component {
   @attribute type = 'tel'
 
   /**
-   * internationalPhoneNumber
-   * @property number
-   * @return {String}
-   * @default null
-   */
+    The international phone number. This is the main data supposed
+    to be persisted / handled.
+
+    @argument number
+    @type {string}
+  */
   number = this.number || null
 
   /**
-   * Add or remove input placeholder with an example number for the selected
-   * country. Possible values are "polite", "aggresive" and "off". Defaults to
-   * "polite".
-   *
-   * @property autoPlaceholder
-   * @return {String}
-   * @default 'polite'
-   */
+    Add or remove input placeholder with an example number for the selected
+    country. Possible values are 'polite', 'aggresive' and 'off'. Defaults to
+    'polite'.
+
+    @argument autoPlaceholder
+    @type {string}
+  */
   autoPlaceholder = this.autoPlaceholder || 'polite'
 
   /**
-   * It will just be the first country in the list. Set the initial country by
-   * it's country code. Defaults to "".
-   *
-   * @property initialCountry
-   * @return {String}
-   * @default ''
-   */
+    It will just be the first country in the list. Set the initial country by
+    it's country code. Defaults to ''.
+
+    @argument initialCountry
+    @type {string}
+  */
   initialCountry = this.initialCountry || ''
 
   /**
-   * Display only the countries you specify -
-   * [see example](http://jackocnr.com/lib/intl-tel-input/examples/gen/only-countries-europe.html).
-   *
-   * @property onlyCountries
-   * @return {Array}
-   * @default undefined
-   */
+    Display only the countries you specify -
+    [see example](http://jackocnr.com/lib/intl-tel-input/examples/gen/only-countries-europe.html).
+
+    @argument onlyCountries
+    @type {Array}
+  */
   onlyCountries = this.onlyCountries || undefined
 
   /**
-   * Specify the countries to appear at the top of the list.
-   *
-   * @property preferredCountries
-   * @return {Array}
-   * @default ['us', 'gb']
-   */
+    Specify the countries to appear at the top of the list.
+
+    @argument preferredCountries
+    @type {Array}
+  */
   preferredCountries = this.preferredCountries || ['us', 'gb']
 
   /**
-   * Implement this function to update the value.
-   *
-   * @method update
-   */
+    You have to implement this function to update the `number`.
+
+    @method update
+    @param {string} number The international phoneNumber
+    @param {Object} metadata The phoneNumber metadata
+    @param {string} metadata.extension The extension part of the current number, so if the number was '+1 (702) 123-1234 ext. 12345' this would return '12345'.
+    @param {Object} metadata.selectedCountryData The country data for the currently selected flag.
+    @param {boolean} metadata.isValidNumber The validity of the current `phoneNumber`.
+    @param {string} metadata.validationError The validation errors
+  */
   update = this.update || function() {}
 
   init() {
@@ -80,7 +91,7 @@ export default class IntlTelInput extends Component {
       this.autoPlaceholder
     )
     assert(
-      '`autoPlaceholder` possible values are "polite", "aggresive" and "off"',
+      "`autoPlaceholder` possible values are 'polite', 'aggresive' and 'off'",
       !validAutoPlaceholer
     )
   }
@@ -92,39 +103,14 @@ export default class IntlTelInput extends Component {
       format
     )
 
-    /**
-     * Get the extension part of the current number, so if the number was
-     * "+1 (702) 123-1234 ext. 12345" this would return "12345".
-     *
-     * @property extension
-     * @return {String}
-     */
     const extension = $(this.element).intlTelInput('getExtension')
 
-    /**
-     * Get the country data for the currently selected flag.
-     *
-     * @property selectedCountryData
-     * @return {Object}
-     */
     const selectedCountryData = $(this.element).intlTelInput(
       'getSelectedCountryData'
     )
 
-    /**
-     * Get the validity of the current `phoneNumber`.
-     *
-     * @property isValidNumber
-     * @return {Boolean}
-     */
     const isValidNumber = $(this.element).intlTelInput('isValidNumber')
 
-    /**
-     * Get the validation errors.
-     *
-     * @property validationError
-     * @return {String}
-     */
     const validationError = this._validationError()
 
     this.get('update')(internationalPhoneNumber, {
