74cea13669141f5148ff0a4692e7dd5cf14adca2
Cyrille David <dcyriller@gmail.com>
2018-12-22 22:29:11 +0100

    Fix ember-2.18 test scenario

--- config/ember-try.js
+++ config/ember-try.js
@@ -13,8 +13,14 @@ module.exports = function() {
       scenarios: [
         {
           name: 'ember-lts-2.18',
+          env: {
+            EMBER_OPTIONAL_FEATURES: JSON.stringify({
+              'jquery-integration': true
+            })
+          },
           npm: {
             devDependencies: {
+              '@ember/jquery': '^0.5.1',
               'ember-source': '~2.18.0'
             }
           }
