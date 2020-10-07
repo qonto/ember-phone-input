a2e7fdac92cdc0f501223e32baf96dee1b606320
Philip Dudley <pdud@hey.com>
2020-10-07 18:13:58 -0400

    Ensure subsequent service:phone-input load calls are thenable

---
+++ tests/unit/services/phone-input-test.js
@@ -0,0 +1,20 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
+
+module('Unit | Service | phone-input', function(hooks) {
+  setupTest(hooks);
+
+  test('load is thenable on first and subsequent renders', function(assert) {
+    assert.expect(2);
+
+    let service = this.owner.lookup('service:phone-input');
+
+    service.load().then(() => {
+      assert.ok(true, 'the first load is thenable');
+    });
+
+    service.load().then(() => {
+      assert.ok(true, 'the second load is thenable');
+    });
+  });
+});
