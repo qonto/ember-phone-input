6c8ab8d4ea99ef9043ea6e2032b00244e8ec3f54
Adrien Le Gall <adrien.legall@qonto.com>
2021-04-28 09:35:55 +0200

    Dont refresh the input for an invalid country code

--- addon/components/phone-input.js
+++ addon/components/phone-input.js
@@ -253,9 +253,8 @@ export default Component.extend({
 
     if (selectedCountry.iso2) {
       this._iti.setCountry(selectedCountry.iso2);
+      this.input();
     }
-
-    this.input();
   },
 
   _metaData(iti) {
