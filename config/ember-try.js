abe5c92556d82fdd5189f4fe81067eb49f05cc78
Cyrille David <cyrille@qonto.eu>
2021-10-07 11:18:42 +0200

    Add and activate `embroider-safe` tests
    The purpose it to test this addon against the new build system
    (embroider).
    
    For now, only the `embroider-safe` scenario is activated in CI.

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const getChannelURL = require('ember-source-channel-url');
+const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
 
 module.exports = async function() {
   return {
@@ -46,6 +47,8 @@ module.exports = async function() {
           }
         }
       },
+      embroiderSafe(),
+      embroiderOptimized(),
       {
         name: 'ember-default-with-jquery',
         env: {
