3b03e37da3969a1ff5793f37677cb58779ff8336
Cyrille David <cyrille@qonto.eu>
2021-10-08 09:39:17 +0200

    Remove pod usage (not supported by addon-docs anymore)

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -3,7 +3,6 @@
 module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'dummy',
-    podModulePrefix: 'dummy/pods',
     environment,
     rootURL: '/',
     locationType: 'auto',
